38c759019b4cf5b22602824950038c21bddeb474
keithvenh <keith.venhuizen@gmail.com>
2022-05-21 12:47:52 -1000

    Add Gear and Level to Character Page

--- src/app/components/characters/Character.jsx
+++ src/app/components/characters/Character.jsx
@@ -1,28 +1,41 @@
 function Character(props) {
 
-    const shards = [15, 30, 55, 80, 130, 200]
+    const shards = [15, 30, 55, 80, 130, 200, 300]
     let width = (props.character.shards / shards[props.character.stars]) * 100;
-    console.log(width);
-    let style = {
-        "width": width + "%"
-    }
+    let grey = '#999999';
+    let green = '#33dd33';
+    let blue = '#66ccff';
+    let purple = '#cc00dd';
+    let orange = '#ff9900';
+    let teal = '#00ffaa';
+    let gearColors = [grey, grey, green, green, green, blue, blue, blue, blue, purple, purple, purple, purple, orange, orange, orange, orange, teal, teal]
     return (
         <div className='character'>
-            
-            <h1 className='characterName'>{props.character.name}</h1>
-            <div className='characterStars'>
-                <span className={`star ${props.character.redStars >= 1 ? 'red' : 'yellow'}`}>{props.character.stars >= 1 ? `★`: `☆`}</span>
-                <span className={`star ${props.character.redStars >= 2 ? 'red' : 'yellow'}`}>{props.character.stars >= 2 ? `★`: `☆`}</span>
-                <span className={`star ${props.character.redStars >= 3 ? 'red' : 'yellow'}`}>{props.character.stars >= 3 ? `★`: `☆`}</span>
-                <span className={`star ${props.character.redStars >= 4 ? 'red' : 'yellow'}`}>{props.character.stars >= 4 ? `★`: `☆`}</span>
-                <span className={`star ${props.character.redStars >= 5 ? 'red' : 'yellow'}`}>{props.character.stars >= 5 ? `★`: `☆`}</span>
-                <span className={`star ${props.character.redStars >= 6 ? 'red' : 'yellow'}`}>{props.character.stars >= 6 ? `★`: `☆`}</span>
-                <span className={`star ${props.character.redStars >= 7 ? 'red' : 'yellow'}`}>{props.character.stars >= 7 ? `★`: `☆`}</span>
+            <div className='characterSide'>
+                <p className="characterLevel">{props.character.level}</p>
             </div>
-            <div className='characterShardsContainer'>
-                <div className='characterShards' style={style}>
-                    <p>{props.character.shards} / {shards[props.character.stars]}</p>
+            
+            <div className='characterCenter'>
+
+                <h1 className='characterName'>{props.character.name}</h1>
+                <div className='characterStars'>
+                    <span className={`star ${props.character.redStars >= 1 ? 'red' : 'yellow'}`}>{props.character.stars >= 1 ? `★`: `☆`}</span>
+                    <span className={`star ${props.character.redStars >= 2 ? 'red' : 'yellow'}`}>{props.character.stars >= 2 ? `★`: `☆`}</span>
+                    <span className={`star ${props.character.redStars >= 3 ? 'red' : 'yellow'}`}>{props.character.stars >= 3 ? `★`: `☆`}</span>
+                    <span className={`star ${props.character.redStars >= 4 ? 'red' : 'yellow'}`}>{props.character.stars >= 4 ? `★`: `☆`}</span>
+                    <span className={`star ${props.character.redStars >= 5 ? 'red' : 'yellow'}`}>{props.character.stars >= 5 ? `★`: `☆`}</span>
+                    <span className={`star ${props.character.redStars >= 6 ? 'red' : 'yellow'}`}>{props.character.stars >= 6 ? `★`: `☆`}</span>
+                    <span className={`star ${props.character.redStars >= 7 ? 'red' : 'yellow'}`}>{props.character.stars >= 7 ? `★`: `☆`}</span>
                 </div>
+                <div className='characterShardsContainer'>
+                    <div className='characterShards' style={{ "width": width + "%" }}>
+                        <p>{props.character.shards} / {shards[props.character.stars]}</p>
+                    </div>
+                </div>
+
+            </div>
+            <div className='characterSide'>
+                <p className='characterGear' style={{ "border-color": gearColors[props.character.gear.tier] }}>{props.character.gear.tier}</p>
             </div>
 
         </div>
--- src/app/components/dashboard/Dashboard.jsx
+++ src/app/components/dashboard/Dashboard.jsx
@@ -1,5 +1,39 @@
-function Dashboard() {
-    return <h1>Dashboard</h1>
+import { db } from '../../../db/db';
+
+function Dashboard(props) {
+
+    const charactersCollected = db.filter((char) => { return char.stars > 0 }).length;
+
+
+
+
+
+
+
+    return (
+    
+        <div className='dashboard'>
+            <div  className='characters'>
+                {charactersCollected + " / " + db.length}
+            </div>
+
+            <div className='shards'>
+
+            </div>
+
+            <div className='levels'>
+
+            </div>
+
+            <div className='abilities'>
+
+            </div>
+
+            <div className='iso'>
+
+            </div>
+        </div>
+    )
 }
 
 export default Dashboard;
